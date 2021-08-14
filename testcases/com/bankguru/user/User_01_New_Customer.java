2fdf17262557357ef58f661401eab3aa50f2fd1b
Henry Nguyen <nguyenhh2703@gmail.com>
2021-08-14 15:24:34 +0700

    Init new automation test framework

---
+++ testcases/com/bankguru/user/User_01_New_Customer.java
@@ -0,0 +1,36 @@
+package com.bankguru.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User_01_New_Customer {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void New_Customer_01_Validate_Name() {
+	}
+
+	@Test
+	public void New_Customer_02_Validate_Phone() {
+	}
+
+	@Test
+	public void New_Customer_03_Name_Validate_City() {
+	}
+
+	@Test
+	public void New_Customer_04_Name_Validate_State() {
+	}
+
+	@Test
+	public void New_Customer_05_Name_Validate_Address() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
---
+++ testdata/com/bankguru/data/payment.java
@@ -0,0 +1,16 @@
+package com.bankguru.data;
+
+public class payment {
+	public class FundTransfer{
+		
+	}
+	
+	
+	public class Deposit{
+		
+	}
+	
+	public class Withdrawal{
+		
+	}
+}

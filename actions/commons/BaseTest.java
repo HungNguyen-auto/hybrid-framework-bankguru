15b41805ac242c98c60e35cf9ed3ff9c49bb1242
HENRY-NGUYEN <nguyenhh2703@gmail.com>
2021-09-05 15:31:20 +0700

    Customize Assert TestNG

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,67 +1,134 @@
 package commons;
 
-import java.io.File;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BaseTest {
-	private String projectLocation = System.getProperty("user.dir");
-	private WebDriver driver;
-	
-	private enum BROWSER {
-		CHROME, FIREFOX, IE, SAFARI, ED_LEGACY, EDGE_CHROMIUM, H_CHROME, H_FIREFOX;
-	}
-	
-	protected WebDriver getBrowserDriver(String browserName) {
-		BROWSER browser = BROWSER.valueOf(browserName.toUpperCase());
-		if(browser == BROWSER.FIREFOX)
-		{
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-		} else if(browser == BROWSER.CHROME) {
-			WebDriverManager.chromedriver().setup();
+    private String projectLocation = System.getProperty("user.dir");
+    private WebDriver driver;
+
+    protected final Log log;
+
+    protected BaseTest(){
+        log = LogFactory.getLog(getClass());
+    }
+    private enum BROWSER {
+        CHROME, FIREFOX, IE, SAFARI, ED_LEGACY, EDGE_CHROMIUM, H_CHROME, H_FIREFOX;
+    }
+
+    protected WebDriver getBrowserDriver(String browserName) {
+        BROWSER browser = BROWSER.valueOf(browserName.toUpperCase());
+        if (browser == BROWSER.FIREFOX) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browser == BROWSER.CHROME) {
+            WebDriverManager.chromedriver().setup();
 //			File file = new File(projectLocation + "\\browserExtensions\\adblock.crx");
 //			ChromeOptions options = new ChromeOptions();
 //			options.addExtensions(file);
-			driver = new ChromeDriver();
-		} else if(browser == BROWSER.EDGE_CHROMIUM){
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-		} else {
-			throw new RuntimeException("Please enter browser name!");
-		}
-		return driver;
-	}
-	protected WebDriver getBrowserDriver(String browserName, String url) {
-		BROWSER browser = BROWSER.valueOf(browserName.toUpperCase());
-		if(browser == BROWSER.FIREFOX)
-		{
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-		} else if(browser == BROWSER.CHROME) {
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-		} else if(browser == BROWSER.EDGE_CHROMIUM){
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-		} else {
-			throw new RuntimeException("Please enter browser name!");
-		}
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get(url);
-		driver.manage().window().maximize();
-		return driver;
-	}
-	
-	public String generateRandomEmail() {
-		Random rand = new Random();
-		return "henry" + rand.nextInt(9999) + "@gmail.com";
-}
+            driver = new ChromeDriver();
+        } else if (browser == BROWSER.EDGE_CHROMIUM) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            throw new RuntimeException("Please enter browser name!");
+        }
+        return driver;
+    }
+
+    protected WebDriver getBrowserDriver(String browserName, String url) {
+        BROWSER browser = BROWSER.valueOf(browserName.toUpperCase());
+        if (browser == BROWSER.FIREFOX) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browser == BROWSER.CHROME) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser == BROWSER.EDGE_CHROMIUM) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            throw new RuntimeException("Please enter browser name!");
+        }
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get(url);
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public String generateRandomEmail() {
+        Random rand = new Random();
+        return "henry" + rand.nextInt(9999) + "@gmail.com";
+    }
+    private boolean checkTrue(boolean condition) {
+        boolean pass = true;
+        try {
+            if (condition == true) {
+                log.info(" -------------------------- PASSED -------------------------- ");
+            } else {
+                log.info(" -------------------------- FAILED -------------------------- ");
+            }
+            Assert.assertTrue(condition);
+        } catch (Throwable e) {
+            pass = false;
+
+            // Add lỗi vào ReportNG
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+
+    protected boolean verifyTrue(boolean condition) {
+        return checkTrue(condition);
+    }
+
+    private boolean checkFailed(boolean condition) {
+        boolean pass = true;
+        try {
+            if (condition == false) {
+                log.info(" -------------------------- PASSED -------------------------- ");
+            } else {
+                log.info(" -------------------------- FAILED -------------------------- ");
+            }
+            Assert.assertFalse(condition);
+        } catch (Throwable e) {
+            pass = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+
+    protected boolean verifyFalse(boolean condition) {
+        return checkFailed(condition);
+    }
+
+    private boolean checkEquals(Object actual, Object expected) {
+        boolean pass = true;
+        try {
+            Assert.assertEquals(actual, expected);
+            log.info(" -------------------------- PASSED -------------------------- ");
+        } catch (Throwable e) {
+            pass = false;
+            log.info(" -------------------------- FAILED -------------------------- ");
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+
+    protected boolean verifyEquals(Object actual, Object expected) {
+        return checkEquals(actual, expected);
+    }
 }
---
+++ testcases/com/facebook/Level_12_Register_Assert_Verify.java
@@ -0,0 +1,49 @@
+package com.facebook;
+
+import commons.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageObjects.facebook.PageGeneratorManager;
+import pageObjects.facebook.RegisterPageObject;
+
+public class Level_12_Register_Assert_Verify extends BaseTest {
+    WebDriver driver;
+    RegisterPageObject registerPage;
+
+    @Parameters({"browser", "url"})
+    @BeforeClass
+    public void beforeClass(String browserName, String appUrl) {
+        driver = getBrowserDriver(browserName, appUrl);
+        registerPage = PageGeneratorManager.getRegisterPage(driver);
+    }
+
+    @Test
+    public void Register_01_Element_Displayed() {
+        // Failed lan 1
+        verifyFalse(registerPage.isEmailTextboxDisplayed());
+        registerPage.inputToEmailTextbox("henry@gmail.com");
+        registerPage.sleepInSecond(2);
+
+        //Fail lan 2
+        verifyFalse(registerPage.isConfirmEmailTextboxDisplayed());
+        registerPage.inputToEmailTextbox("");
+        // Undisplayed: Invisible on UI + Exist in DOM
+        registerPage.sleepInSecond(2);
+        verifyFalse(registerPage.isConfirmEmailTextboxDisplayed());
+
+        verifyTrue(registerPage.isConfirmEmailTextboxUndisplayed());
+
+        verifyFalse(registerPage.isLoginButtonDisplayed());
+
+        // Failed lan 3
+        verifyFalse(registerPage.isLoginButtonUndisplayed());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
